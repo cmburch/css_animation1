7cd723afdbd24387bc3cdac7263d79b0a0fdc48c
Charles Burch <georgia_beast89@yahoo.com>
2019-05-08 17:20:41 -0500

    setup up the routes in the app.js file still nedd to work on the navbar next

--- resort/src/App.js
+++ resort/src/App.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import './App.css';
+
 import Home from './pages/Home';
 import Rooms from './pages/Rooms';
 import SingleRoom from './pages/SingleRoom';
 import Error from './pages/Error';
 
+import { Switch, Route } from "react-router-dom";
+
+
 
 function App() {
   return (
-    <div>
-      <Home></Home>
-      <Rooms></Rooms>
-      <SingleRoom></SingleRoom>
-      <Error></Error>
-    </div>
+    <Switch>
+    <Route exact path="/" component={Home} />
+    <Route exact path="/rooms/" component={Rooms} />
+    <Route exact path="/rooms/:id" component={SingleRoom} />
+    <Route component={Error} />
+  </Switch>
   );
 }
 
