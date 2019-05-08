06691c334292862cad43cb2b5e401fcfed22dd40
Charles Burch <georgia_beast89@yahoo.com>
2019-05-08 16:49:11 -0500

    fixed minor error within the page component ,the pages are in the initial setup phase

--- resort/src/pages/Rooms.jsx
+++ resort/src/pages/Rooms.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Room = () => {
+const Rooms = () => {
   return (
     <div>
       Room Page
@@ -8,4 +8,4 @@ const Room = () => {
   )
 }
 
-export default Room
+export default Rooms
