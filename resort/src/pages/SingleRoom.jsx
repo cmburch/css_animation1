723eb3e9677af428c460dd648b932a18c2f33eab
Charles Burch <georgia_beast89@yahoo.com>
2019-05-09 14:25:02 -0500

    created a styled header component and fixed a routing issue

--- resort/src/pages/SingleRoom.jsx
+++ resort/src/pages/SingleRoom.jsx
@@ -4,6 +4,8 @@ import Hero from "../components/Hero";
 import Banner from "../components/Banner";
 import { Link } from "react-router-dom";
 import { RoomContext } from "../Context";
+import StyledHero from "../components/StyledHero";
+
 class SingleRoom extends Component {
 
   constructor(props) {
@@ -44,14 +46,13 @@ class SingleRoom extends Component {
     } = room;
     return (
       <>
-        <Hero hero="roomsHero">
+        <StyledHero img={images[0] || this.state.defaultBcg}>
           <Banner title={`${name} room`}>
-            <Link to="/rooms" 
-            className="btn-primary">
+            <Link to="/rooms" className="btn-primary">
               back to rooms
             </Link>
           </Banner>
-        </Hero>
+        </StyledHero>
       </>
     )
   }
