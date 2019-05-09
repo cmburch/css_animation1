4b41aa8f5b68dafbbf43e85d7d5bfa104d6be6ec
Charles Burch <georgia_beast89@yahoo.com>
2019-05-09 11:02:55 -0500

    created mapping method to structure the room data into usable object

--- resort/src/Context.jsx
+++ resort/src/Context.jsx
@@ -1,11 +1,31 @@
 import React, { Component } from 'react'
+import items from "./data";
 
 const RoomContext = React.createContext();
 class RoomProvider extends Component {
     state = {
-        greeting: 'Hello',
-        name: 'Me'
+        rooms: [],
+        sortedRooms: [],
+        featuredRooms: [],
+        loading: true
     } 
+
+    componentDidMount() {
+        let rooms = this.formatData(items);
+        console.log(rooms);
+    }
+    formatData(items) {
+        let tempItems = items.map(item => {
+          let id = item.sys.id;
+          //list of images
+          let images = item.fields.images.map(image => image.fields.file.url);
+    
+          let room = { ...item.fields, images, id };
+          return room;
+        });
+        return tempItems;
+      }
+
   render() {
     return (
         <RoomContext.Provider value={{...this.state}}>
