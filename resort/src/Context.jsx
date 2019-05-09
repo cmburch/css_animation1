d9a038d273714fd837edf5b2df7175afe67ecde5
Charles Burch <georgia_beast89@yahoo.com>
2019-05-09 13:32:40 -0500

    added the header section to the single room page ,need to dynamically style so I will look at other options later

--- resort/src/Context.jsx
+++ resort/src/Context.jsx
@@ -41,9 +41,7 @@ class RoomProvider extends Component {
 
   render() {
     return (
-        <RoomContext.Provider value={{...this.state,
-            getRoom:this.getRoom
-        }}>
+        <RoomContext.Provider value={{...this.state, getRoom : this.getRoom}}>
             {this.props.children}
         </RoomContext.Provider>
     )
