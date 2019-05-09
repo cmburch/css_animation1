a104b2572cd64a2f68845ebbc3f7d9e39eb78c9d
Charles Burch <georgia_beast89@yahoo.com>
2019-05-09 09:03:43 -0500

    initial setup of the context Api for the rooms

--- resort/src/index.js
+++ resort/src/index.js
@@ -4,13 +4,18 @@ import './index.css';
 import App from './App';
 import { BrowserRouter as Router} from "react-router-dom";
 
+import { RoomProvider } from "./Context";
+
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
+  <RoomProvider>
     <Router>
-        <App />
-    </Router>,
- document.getElementById('root'));
+      <App />
+    </Router>
+  </RoomProvider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
