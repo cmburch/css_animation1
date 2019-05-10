39231919765ec33611ba0d05618b733a43f54f45
Charles Burch <georgia_beast89@yahoo.com>
2019-05-09 19:21:01 -0500

    created the layout for the filter and room type filter drop with the css ,need to work on the methods now

--- resort/src/components/RoomsFilter.jsx
+++ resort/src/components/RoomsFilter.jsx
@@ -1,11 +1,62 @@
-import React from 'react'
+import React from 'react';
+import { useContext } from "react";
+import { RoomContext } from "../Context";
+import Title from "./Title";
 
-const RoomsFilter = () => {
+// get all unique values
+const getUnique = (items, value) => {
+    return [...new Set(items.map(item => item[value]))];
+};
+
+const RoomsFilter = ({ rooms }) => {
+
+      // react hooks
+  const context = useContext(RoomContext);
+  const {
+    handleChange,
+    type,
+    capacity,
+    price,
+    minPrice,
+    maxPrice,
+    minSize,
+    maxSize,
+    breakfast,
+    pets
+  } = context;
+
+    // get unique types
+    let types = getUnique(rooms, "type");
+    // add all
+    types = ["all", ...types];
+    // map to jsx
+    types = types.map((item, index) => (
+      <option key={index} value={item}>
+        {item}
+      </option>
+    ));
+
   return (
-    <div>
-       Room Filiter componenet
-    </div>
-  )
+    <section className="filter-container">
+      <Title title="search rooms" />
+      <form className="filter-form">
+        {/* select type */}
+        <div className="form-group">
+          <label htmlFor="type">room type</label>
+          <select
+            name="type"
+            id="type"
+            onChange={handleChange}
+            className="form-control"
+            value={type}
+          >
+            {types}
+          </select>
+        </div>
+        {/* end of select type */}
+      </form>
+    </section>
+  );
 }
 
 export default RoomsFilter
