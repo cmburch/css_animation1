f0a16ae05d69ac33772181421186711caacb0fdb
Charles Burch <georgia_beast89@yahoo.com>
2019-05-09 19:10:17 -0500

    created the setup for the filitering of rooms in the room context Api and room type dropdown still needs work

--- resort/src/Context.jsx
+++ resort/src/Context.jsx
@@ -7,18 +7,37 @@ class RoomProvider extends Component {
         rooms: [],
         sortedRooms: [],
         featuredRooms: [],
-        loading: true
+        loading: true,
+
+        //for filiter
+        type: "all",
+        capacity: 1,
+        price: 0,
+        minPrice: 0,
+        maxPrice: 0,
+        minSize: 0,
+        maxSize: 0,
+        breakfast: false,
+        pets: false
     } 
 
     componentDidMount() {
         let rooms = this.formatData(items);
         let featuredRooms = rooms.filter(room => room.featured === true);
 
+        let maxPrice = Math.max(...rooms.map(item => item.price));
+        let maxSize = Math.max(...rooms.map(item => item.size));
+
         this.setState({
           rooms,
           featuredRooms,
           sortedRooms: rooms,
-          loading: false
+          loading: false,
+
+          //
+          price: maxPrice,
+          maxPrice,
+          maxSize
         });
     }
     formatData(items) {
@@ -39,9 +58,31 @@ class RoomProvider extends Component {
         return room;
       };
 
+      handleChange = event => {
+        //method is used for filitering rooms
+        const target = event.target;
+        const value = target.type === "checkbox" ? target.checked : target.value;
+        const name = target.name;
+        console.log(name, value);
+    
+        // this.setState(
+        //   {
+        //     [name]: value
+        //   },
+        //   this.filterRooms
+        // );
+      };
+
+    filterRooms = () => {
+        console.log('filtered rooms');
+    } 
+
   render() {
     return (
-        <RoomContext.Provider value={{...this.state, getRoom : this.getRoom}}>
+        <RoomContext.Provider value={{...this.state, 
+        getRoom : this.getRoom,
+        handleChange: this.handleChange
+        }}>
             {this.props.children}
         </RoomContext.Provider>
     )
